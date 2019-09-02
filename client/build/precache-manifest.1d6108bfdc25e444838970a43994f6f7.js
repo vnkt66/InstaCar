fe5dab326bff189140f1d8650f8e4949dd4ed158
venkat <penubothuvenkannachowdary@gmail.com>
2019-09-02 23:18:53 +0530

    changed

---
+++ client/build/precache-manifest.1d6108bfdc25e444838970a43994f6f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1a3a3b61126da74b8134fdbca4d38d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "316ce61f092cc518a2bc",
+    "url": "/static/css/2.4648fcd9.chunk.css"
+  },
+  {
+    "revision": "b54d0f15ecbf244f09d2",
+    "url": "/static/css/main.a026c1c1.chunk.css"
+  },
+  {
+    "revision": "316ce61f092cc518a2bc",
+    "url": "/static/js/2.d9cec295.chunk.js"
+  },
+  {
+    "revision": "b54d0f15ecbf244f09d2",
+    "url": "/static/js/main.a2f327cb.chunk.js"
+  },
+  {
+    "revision": "68db10f962e109d557fb",
+    "url": "/static/js/runtime~main.701562a6.js"
+  }
+]);
